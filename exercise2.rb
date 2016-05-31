18db82e0a161ea0a801ac64f46586d0999c87692
thandon263 <ncubethando20@gmail.com>
2016-05-31 14:53:03 -0400

    exercise 2 for operators, tenary, logical and boolean

--- exercise2.rb
+++ exercise2.rb
@@ -1,1 +1,10 @@
 puts 55 * 0.13
+
+# adding a number to a string
+day = 365
+p "The last day of the year would be " + day.to_s + " in a normal year."
+
+# multiplying two numbers
+p "In a chest of treasure you can get #{45628 * 7839} dollars let\'s find it"
+
+p (10 < 20 && 30 < 20)|| !(10 == 11) # true
